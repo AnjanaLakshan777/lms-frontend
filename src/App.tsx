30d0f9705474ad50f244ee9979fd5a0784a077e8
AnjanaLakshan777 <anjanalakshan777@gmail.com>
2026-09-06 23:28:50 +0530

    add .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "./components/Navbar/NavBar";
 import { Student } from "./components/Student";
 import { Instructor } from "./components/Instructor";
 import { Course } from "./components/course/Course";
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/lms/api/v1",
+    baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
